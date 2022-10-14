54279ec2563560e00cd1409255dd92277eb43367
ireneangulo <irene.anguloeso@gmail.com>
2022-10-14 12:43:19 +0200

    he hecho parte del ejrcicio

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,4 +1,7 @@
-numeros=(3,16,87,7,3,14,-68,1,54,16,7,4,25)
+numeros=[3,16,87,7,3,14,-68,1,54,16,7,4,25]
 def modificar(lista):
     lista= list(set(lista))
     lista.sort(reverse=True) #Ordena los numeros que hay en la lista llamada numeros
+    #print(lista)
+    lista_temporal=[]
+
