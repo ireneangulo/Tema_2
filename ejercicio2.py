78c70110ed7e4d96ae949e7a7ee8f51355ce7507
ireneangulo <irene.anguloeso@gmail.com>
2022-10-14 13:04:57 +0200

    he hecho parte del ejercicio

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,2 +1,4 @@
 texto= ("un día que el viento soplaba con fuerza#mira como se mueve aquella banderola -dijo un monje#lo que se mueve es el viento -respondió otro monje#ni las banderolas ni el viento, lo que se mueve son vuestras mentes -dijo el maestro")
 frases= texto.split(sep= "#") #Separa cada vez que hay un #
+for f, frase in enumerate(frases):
+    frases[f]= frase.title()
