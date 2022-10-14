1a312c77ad12c8e6c49078997620bbc3d48eae89
ireneangulo <irene.anguloeso@gmail.com>
2022-10-14 13:59:35 +0200

    he definido una funcion

--- ejercicio1.py
+++ ejercicio1.py
@@ -6,3 +6,20 @@ class Punto:
     
     def __str__(self):
         return "({}, {})".format(self.x, self.y)
+
+    def cuadrante (self):
+        if self.x >0 and self.y >0:
+            print("{} pertenece al primer cuadrante".format(self))
+        elif self.x <0 and self.y >0:
+            print("{} pertenece al segundo cuadrante".format(self))
+        elif self.x <0 and self.y <0:
+            print("{} pertenece al tercer cuadrante".format(self))
+        elif self.x >0 and self.y <0:
+            print("{} pertenece al cuarto cuadrante".format(self))
+        elif self.x !=0 and self.y ==0:
+            print("{} se sitúa sobre el eje x".format(self))
+        elif self.x ==0 and self.y !=0:
+            print("{} se situa sobre el eje y".format(self))
+        else:
+            print("{}se encuentra sobre el origen".format(self))
+        
