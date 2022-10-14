781c261742271bb929e2de23edfc6d89dc8c885d
ireneangulo <irene.anguloeso@gmail.com>
2022-10-14 14:42:26 +0200

    he cambiado el ejercicio

--- ejercicio2.py
+++ ejercicio2.py
@@ -3,6 +3,8 @@ frases= texto.split(sep= "#") #Separa cada vez que hay un #
 for f, frase in enumerate(frases):
     frases[f]= frase.capitalize() #pone la primera letra de la frase en mayúscula
     if f == 0:
+        frases[f] = frases[f] + "..."
+    else:
         frases[f] = " • " + frases[f] + "." #Añade el punto para enumerar las tres frases, y el punto del final
 for frase in frases:
     print(frases)
