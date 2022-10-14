504ebbccdd3a8d84cbc65cdd971c6a1bc0c7fb69
ireneangulo <irene.anguloeso@gmail.com>
2022-10-14 12:54:39 +0200

    he hecho el ejercicio

--- ejercicio3.py
+++ ejercicio3.py
@@ -10,8 +10,12 @@ def modificar(lista):
     print(lista)
     print(sorted(lista, reverse=True)) #utilizamos reverse para que la lista se ordene de mayor a menor
 
-lista_temporal= []
-for numero in lista:
-    if numero%2 ==0:
-        lista_temporal.append(numero)
-        print(lista_temporal)
+    lista_temporal= []
+    for numero in lista:
+        if numero%2 ==0:
+            lista_temporal.append(numero)
+            print(lista_temporal)
+    suma_numeros= sum(lista_temporal)
+    lista_temporal.insert(0, suma_numeros)
+    return lista_temporal
+
