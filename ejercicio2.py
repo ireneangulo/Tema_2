f34f5bcf5e196d9fc6f87407cd2fdfdf8633430b
ireneangulo <irene.anguloeso@gmail.com>
2022-10-14 13:08:33 +0200

    he acabado el ejercicio

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,4 +1,8 @@
 texto= ("un día que el viento soplaba con fuerza#mira como se mueve aquella banderola -dijo un monje#lo que se mueve es el viento -respondió otro monje#ni las banderolas ni el viento, lo que se mueve son vuestras mentes -dijo el maestro")
 frases= texto.split(sep= "#") #Separa cada vez que hay un #
 for f, frase in enumerate(frases):
-    frases[f]= frase.capitalize()
+    frases[f]= frase.capitalize() #pone la primera letra de la frase en mayúscula
+    if f == 0:
+        frases[f] = " • " + frases[f] + "." #Añade el punto para enumerar las tres frases, y el punto del final
+for frase in frases:
+    print(frases)
